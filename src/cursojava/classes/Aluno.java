c86fc9a6a2ba43e762678a39d20b18d159634e0a
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-12-22 22:01:51 -0300

    Iniciando com orientação a objetos

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -2,17 +2,53 @@ package cursojava.classes;
 
 public class Aluno {
 	
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
 	
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
 	public Aluno(String nomePadrao) {
 		nome = nomePadrao;
 	}	
@@ -21,5 +57,94 @@ public class Aluno {
 		
 	}
 	
+	public Aluno(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4)/4;
+	}
 
 }
--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,13 +6,29 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		/*Agora temos o objeto real na memória*/
-		
-		Aluno aluno1 = new Aluno("Wyltamar");
-		
+
+		/* Agora temos o objeto real na memória */
+
+
 		Aluno aluno2 = new Aluno();
 		
+		aluno2.setNome("Wyltamar");
+		aluno2.setIdade(37);
+		aluno2.setNomeMae("Maria do Socorro");
+		aluno2.setNota1(80.0);
+		aluno2.setNota2(90.0);
+		aluno2.setNota3(100.0);
+		aluno2.setNota4(80.0);
+		
+		System.out.println("O nome do aluno é:  " + aluno2.getNome());
+		System.out.println("A idade do auno é: " + aluno2.getIdade());
+		System.out.println("O nome da mãe é: "+aluno2.getNomeMae());
+		System.out.println("Nota 1 = "+aluno2.getNota1());
+		System.out.println("Nota 2 = "+aluno2.getNota2());
+		System.out.println("Nota 3 = "+aluno2.getNota3());
+		System.out.println("Nota 4 = "+aluno2.getNota4());
+		System.out.println("================================================");
+		System.out.println("Média final do aluno = "+aluno2.getMediaNota());
 		
 
 	}
